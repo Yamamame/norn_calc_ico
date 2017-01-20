d76273350ac3e8aa381d8a75a1ed8a2fca2eef52
n.koyama <koyama@alion88.com>
2017-01-20 10:01:37 +0900

    Opencvライブラリ実行成功
    Signed-off-by: n.koyama <koyama@alion88.com>

--- opencv_testcmd.py
+++ opencv_testcmd.py
@@ -1,19 +1,49 @@
 #!/usr/bin/python
 #coding: utf-8
-
+#opencv_testcmd.py
 import cv2
 import numpy as np
+# enable debugging
+from jinja2 import Environment,FileSystemLoader
+import cgitb
+import cgi
+import os
+import sys
 
-faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+cnt = 0
+msg = ''
+status = []
+#filename = '5ac8989e-8710-214d-1c2a-008476953a94.jpeg'
+#filename = '2013-05-18_18-35-27_785.jpg'
+filename = '../../Desktop/yoroshikune/FB_IMG_1471088127162.jpg'
+#filename = '../../Desktop/yoroshikune/1404138199814.jpg'
 
-img = cv2.imread('../../Desktop/1404138199814.jpg', cv2.IMREAD_COLOR)
+#HAAR分類器の顔検出用の特徴量
+#cascade_path = "/usr/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml"
+#cascade_path = "/usr/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml"
+#cascade_path = "/usr/share/OpenCV/haarcascades/haarcascade_frontalface_alt2.xml"
+cascade_path = "/usr/share/OpenCV/haarcascades/haarcascade_frontalface_alt_tree.xml"
+
+faceCascade = cv2.CascadeClassifier(cascade_path)
+
+img = cv2.imread(filename)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 face = faceCascade.detectMultiScale(gray, 1.1, 3)
 
 if len(face) > 0:
 	for rect in face:
+		cnt+=1
 		cv2.rectangle(img, tuple(rect[0:2]), tuple(rect[0:2]+rect[2:4]), (0, 0,255), thickness=2)
+#		status[cnt] = tuple(rect[0:2])
+		status.append({ 'lt':tuple(rect[0:2]) , 'cube':tuple(rect[2:4])})
 else:
-	print "no face"
+	msg = "no face"
 
 cv2.imwrite('detected.jpg', img)
+cgitb.enable()
+env = Environment(loader=FileSystemLoader('./',encoding='utf8'))
+tpl = env.get_template('opencv_test.tpl.html')
+html = tpl.render({'filename':filename,'message':msg,'count':cnt,'rect':status})
+print "Content-Type: text/html;charset=utf-8"
+print ""
+print html.encode('utf8')
