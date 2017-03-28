27185cdc2d7a737235c2af325f3962932d138fe6
n.koyama <koyama@alion88.com>
2017-03-28 10:34:31 +0900

    [fix]parsemethodとりあえずのベースだけ完成

--- source/parse_regex_method.py
+++ source/parse_regex_method.py
@@ -3,3 +3,11 @@ import cgitb
 import cgi
 import os
 import sys
+
+template_filename = './template/parse_regex_method.ptl.html'
+
+
+html = tpl.render({'filename':filename,'message':msg,'count':cnt,'rect':status})
+print "Content-Type: text/html;charset=utf-8"
+print ""
+print html.encode('utf8')
