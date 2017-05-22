6dd876b599430be47c0a3a47c977accdd2e131d5
K.Noboru <yamamame@gmail.com>
2017-05-23 00:25:59 +0900

    [fix]ファイル名間違い直し,処理追加

--- source/parse_regex_method.py
+++ source/parse_regex_method.py
@@ -1,13 +1,20 @@
+#!/usr/bin/python
+#coding: utf-8
+#opencv_testcmd.py
 from jinja2 import Environment,FileSystemLoader
 import cgitb
 import cgi
 import os
 import sys
 
-template_filename = './template/parse_regex_method.ptl.html'
-
-
-html = tpl.render({'filename':filename,'message':msg,'count':cnt,'rect':status})
+template_filename = './template/parse_regex_method.tpl.html'
+msg = ''
+changetxt = ''
+status = ''
+#create HTMl
+env = Environment(loader=FileSystemLoader('./',encoding='utf8'))
+tpl = env.get_template(template_filename)
+html = tpl.render({'message':msg,'changetxt':changetxt,'rect':status})
 print "Content-Type: text/html;charset=utf-8"
 print ""
 print html.encode('utf8')
