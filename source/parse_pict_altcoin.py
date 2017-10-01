32025e993fc70d69b3f457a3ce86e312528a75d9
n.koyama <koyama@alion88.com>
2017-10-01 15:33:34 +0900

    [fix]とりあえず、動くがファイルが取得できない

--- source/parse_pict_altcoin.py
+++ source/parse_pict_altcoin.py
@@ -7,11 +7,11 @@ import cgi
 import os
 import sys
 from pprint import pprint
-
-#画像を解析する
+#analyze picture
 import cv2
 
-template_filename = './template/parse_regex_method.tpl.html'
+template_filename = './template/parse_pict_altcoin.html'
+file_save_dir     = './altcoin/'
 msg = ''
 changetxt = ''
 status = ''
@@ -20,6 +20,20 @@ vd = {}
 v  = []
 # reciept file
 rfile = ''
+
+#HAAR分類器の顔検出用の特徴量
+cascade_list=list()
+cascade_list.append("/usr/share/OpenCV/haarcascades/haarcascade_frontalface_alt_tree.xml")
+cascade_list.append("/usr/share/OpenCV/haarcascades/haarcascade_frontalface_alt2.xml")
+cascade_list.append("/usr/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml")
+cascade_list.append("/usr/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml")
+cascade_path = cascade_list[0]
+
+faceCascade = cv2.CascadeClassifier(cascade_path)
+
+#画像保存用のディレクトリ作成
+if os.path.isdir(file_save_dir) == False :
+    os.mkdir(file_save_dir)
 
 # HTTP Request
 if os.environ['REQUEST_METHOD'] == 'POST' :
@@ -32,18 +46,28 @@ formitem = []
 
 for key in form.keys() :
     vd[key]=form.getvalue(key,'')
+    item_error_msg = ''
     if form[key].file:
         # It's an uploaded file; count lines
         formitem = form[key]
-        linecount = 0
-        while True:
-            line = formitem.file.readline()
-            if not line: break
-            linecount = linecount + 1
-            changetxt += line.decode("utf-8")
-        v.append({ 'key':'i' + key , 'val':formitem.filename })
+        img = cv2.imread(formitem.filename, cv2.IMREAD_UNCHANGED)
+        if img is None:
+            if os.path.exists(formitem.filename) == False :
+                item_error_msg = formitem.filename +  ' file not found !!'
+            else :
+                item_error_msg = formitem.filename +  ' Is Error !!'
+        else :
+            #pict processing
+            height = img.shape[0]
+            width = img.shape[1]
+            cv2.rectangle(img, (2,10), (width - 2, height - 2), (0, 0,255), thickness=2)
+            cv2.imwrite(file_save_dir + formitem.filename, img)
+        v.append({ 'key':'i' + key , 'val':formitem.filename , 'err':item_error_msg })
     else :
-        v.append({ 'key':'e' + key , 'val':form[key].value })
+        item_error_msg = 'File not found !!'
+        v.append({ 'key':'e' + key , 'val':form[key].value , 'err':item_error_msg })
+
+
 
 #create HTMl
 env = Environment(loader=FileSystemLoader('./',encoding='utf8'))
