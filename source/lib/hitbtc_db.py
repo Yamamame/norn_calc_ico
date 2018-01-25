692dc2d83776d7b885a383c122cd5ac51abfa627
n.koyama <koyama@alion88.com>
2018-01-25 17:09:02 +0900

    [fix]volume,totalを計算するように改良

--- source/lib/hitbtc_db.py
+++ source/lib/hitbtc_db.py
@@ -33,6 +33,11 @@ class HITBTCDB(object):
             )
             self.cursor.execute(current_sql,placehold)
             data_one = self.cursor.fetchall()
+            data_row['volume'] = str(float(data_row['quantity']) * float(data_row['price']))
+            if 'buy' in data_row['side'] :
+                data_row['total']  = str((float(data_row['volume']) * -1) - float(data_row['fee']))
+            else :
+                data_row['total']  = str((float(data_row['volume']) * 1) - float(data_row['fee']))
             if debug != 0 :
                 print len(data_one)
                 print('data row : "%s"' % data_row)
@@ -42,16 +47,16 @@ class HITBTCDB(object):
                     {ord(u'T'): u' ',ord(u'Z'): u' ',}
                     ),data_row['symbol'],data_row['id'],
                     data_row['orderId'],data_row['side'],data_row['quantity'],data_row['price'],
-                    data_row['fee'],
+                    data_row['volume'],data_row['fee'],data_row['total'],
                 )
                 current_sql  = ' INSERT INTO t_trades '
                 current_sql += ' (exec_date,instrument,id'
                 current_sql += ' ,order_id,side,quantity,price'
                 # current_sql += ' ,volume,fee,rebate,total,uptime)'
-                current_sql += ' ,fee,uptime)'
+                current_sql += ' ,volume,fee,total,uptime)'
                 # current_sql += ' VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,now())'
                 current_sql += ' VALUES (CONVERT_TZ(%s,"+00:00","+09:00"),%s,%s'
-                current_sql += ',%s,%s,%s,%s,%s,now())'
+                current_sql += ',%s,%s,%s,%s,%s,%s,%s,now())'
                 self.cursor.execute(current_sql,placehold)
                 result = self.cursor.fetchall()
                 # print('xINSERT OK : "%s"' % result)
@@ -63,14 +68,14 @@ class HITBTCDB(object):
                     {ord(u'T'): u' ',ord(u'Z'): u' ',}
                     ),data_row['symbol'],
                     data_row['orderId'],data_row['side'],data_row['quantity'],data_row['price'],
-                    data_row['fee'],
+                    data_row['volume'],data_row['fee'],data_row['total'],
                     data_row['id'],
                 )
                 current_sql  = ' UPDATE t_trades SET '
                 current_sql += ' exec_date = CONVERT_TZ(%s,"+00:00","+09:00"),instrument=%s'
                 current_sql += ' ,order_id=%s,side=%s,quantity=%s,price=%s'
                 # current_sql += ' ,volume=%s,fee=%s,rebate=%s,total=%s,uptime=now()'
-                current_sql += ' ,fee=%s,uptime=now()'
+                current_sql += ' ,volume=%s,fee=%s,total=%s,uptime=now()'
                 current_sql += ' WHERE id=%s '
                 self.cursor.execute(current_sql,placehold)
                 result = self.cursor.fetchall()
