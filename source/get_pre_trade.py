cb793ee416c330dc0d694a789c78eb79ffdd3fd9
K.Noboru <yamamame@gmail.com>
2018-04-16 15:21:42 +0900

    [add]前回取引取得処理追加

---
+++ source/get_pre_trade.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+#coding: utf-8
+#get_pre_trade.py
+#hitbtc    : hitbtcからデータを取得するライブラリ
+#hitbtc_db : 取得したデータを格納するライブラリ
+#参照: https://github.com/hitbtc-com/hitbtc-api/blob/master/example_rest.py
+import sys
+sys.path.append('/home/yama/public_html/py_practice/lib/')
+import numpy as np
+import pandas as pd
+import matplotlib.pylab as plt
+import seaborn as sns
+import warnings
+import hitbtc
+import hitbtc_db
+
+data_dir = "/home/yama/hitbtc/"
+f = open(data_dir + 'apikey.txt', 'r')
+for line in f :
+    strkeydict = line.split(None)
+    if 'API' in strkeydict[0] :
+      api_pub_keys = strkeydict[1]
+    else :
+      api_sec_keys = strkeydict[1]
+target_rest_url  = "https://api.hitbtc.com"
+client = hitbtc.HITBTClient(target_rest_url, api_pub_keys, api_sec_keys)
+eth_btc = client.get_symbol('ETHBTC')
+address = client.get_address('ETH')     # get eth address for deposit
+print('ETH deposit address: "%s"' % address)
+# history_trades = client.get_history_trades()
+# account_balance = client.get_account_balance()
+# trading_balance = client.get_trading_balance()
+# transaction     = client.get_transaction('')
+# transaction     = client.get_transaction_by_a_month('')
+# print('account balance: "%s"' % account_balance)
+# print('transaction: "%s"' % transaction)
+db_access = hitbtc_db.HITBTCDB()
+pre_trades = db_access.pre_trade_value('ETH')
+for data_row in pre_trades :
+    print('instrument: "%s" price : "%s"  quantity : "%1.6f" ' % (str(data_row[0]),'{:.6f}'.format(data_row[1]),data_row[2]))
