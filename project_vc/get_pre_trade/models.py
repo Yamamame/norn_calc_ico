7af3f5b845ebf654e4b3fc81c7b124f22f780229
koyama <koyama@alion88.com>
2019-06-03 17:39:47 +0900

    [fix]追加して画面を試してみる

--- project_vc/get_pre_trade/models.py
+++ project_vc/get_pre_trade/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+#参照   : https://github.com/hitbtc-com/hitbtc-api/blob/master/example_rest.py
+import requests
+import datetime
+import time
+
+
+class HITBTClient(object):
+    def __init__(self, url, public_key, secret):
+        self.url = url + "/api/2"
+        self.session = requests.session()
+        self.session.auth = (public_key, secret)
+        self.now = datetime.date.today()
