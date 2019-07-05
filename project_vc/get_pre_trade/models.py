09dfc00515547eebcc279d9abcee01ab016ffc2f
N.Koyama <yamamame@gmail.com>
2019-07-05 11:04:08 +0900

    Fix models

--- project_vc/get_pre_trade/models.py
+++ project_vc/get_pre_trade/models.py
@@ -1,15 +1,27 @@
 from django.db import models
 
-# Create your models here.
-#参照   : https://github.com/hitbtc-com/hitbtc-api/blob/master/example_rest.py
-import requests
-import datetime
-import time
-
-
-class HITBTClient(object):
-    def __init__(self, url, public_key, secret):
-        self.url = url + "/api/2"
-        self.session = requests.session()
-        self.session.auth = (public_key, secret)
-        self.now = datetime.date.today()
+class payment_history(models.Model):
+    # execute date
+    exec_date = models.DateTimeField()
+    # operation id part 1
+    operation_id_1 = models.CharField(max_length=15)
+    # operation id part 2
+    operation_id_2 = models.CharField(max_length=15)
+    # operation id part 3
+    operation_id_3 = models.CharField(max_length=15)
+    # operation id part 4
+    operation_id_4 = models.CharField(max_length=15)
+    # operation id part 5
+    operation_id_5 = models.CharField(max_length=15)
+    # type
+    payment_type = models.CharField(max_length=64)
+    # amount
+    amount = models.FloatField()
+    # t_hash
+    t_hash = models.CharField(max_length=256)
+    # main_balance
+    main_balance = models.FloatField()
+    # currency
+    currency = models.CharField(max_length=8)
+    # udate_at
+    update_at =  models.DateTimeField()
